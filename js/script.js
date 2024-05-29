94c12fecda33daec951a183676f2f19a6b88937b
jubileu59599 <161588911+jubileu59599@users.noreply.github.com>
2024-05-29 09:34:40 -0300

    metade feito

--- js/script.js
+++ js/script.js
@@ -2,30 +2,37 @@ var meuFormulario = document.getElementById("formulario");
 meuFormulario.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  //Chama a função somar
-  somar();
+  //Chama a função verificar
+  isBetweenZeroAndNine();
 
 });
 
 
-//Função somar
-function somar() {
-  
+//Função verificar
+function isBetweenZeroAndNine() {
+
   //Captura o que está dentro do primeiro input no formulário
   //Number - Transforma o texto capturado em Numero
-  let primeiro_numero_inteiro = Number(document.getElementById("primeiro_numero_inteiro").value);
-  
-  //Captura o que está dentro do segundo input no formulário
-  //Number - Transforma o texto capturado em Numero
-  let segundo_numero_inteiro = Number(document.getElementById("segundo_numero_inteiro").value); 
-  
-  //Realiza a soma do valor que está na variável "primeiro_numero_inteiro" com o valor que está na
-  //variável "segundo_numero_inteiro"
-  //E a variável "resultado_soma" recebe o valor da soma
-  let resultado_soma = primeiro_numero_inteiro + segundo_numero_inteiro;
+  let numero = Number(document.getElementById("numero").value);
 
-  //Aqui vai escrever no input "resultado_soma" o valor que está armazenado na variável "resultado_soma"
-  document.getElementById("resultado_soma").value = resultado_soma;
-  
-  
+  if (numero >= 0 && number <= 9) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Exemplo de uso:
+
+  var resultado = 0;
+
+  if (isBetweenZeroAndNine()) {
+      resultdo( está entre 0 e 9.)
+  } else {
+    resultado( não está entre 0 e 9.);
+  }
+
+  //Aqui vai escrever no input "resultado" o valor que está armazenado na variável "resultado"
+  document.getElementById("resultado").value = resultado;
+
 }
